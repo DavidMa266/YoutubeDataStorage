676f10c77e5a1c5b0c864a232b38ed1039ad7233
David Ma <dsm266@cornell.edu>
2016-09-16 23:19:48 -0400

    hi

---
+++ content_script.js
@@ -0,0 +1,7 @@
+	var button = document.getElementsByClassName("_mqb _mod")
+	console.log(button)
+	console.log(button[0])
+	button[0].addEventListener("click", function() {
+		alert("Testing ");
+		console.log("Hello");
+});
