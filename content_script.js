9ca2f27ad528a609721adec9d151b6d9b5720f1e
Charlie Su <dragonsmirth@gmail.com>
2016-09-17 17:30:30 -0400

    store objects into google.storage.sync

--- content_script.js
+++ content_script.js
@@ -1,17 +1,24 @@
 
 function add_like_button()
 {
-  var button;
+	var button, title, description;
 
     button = document.getElementsByClassName("like-button-renderer-like-button-unclicked");
     button[0].addEventListener("click", function()
     {
       alert("Clicked Like!");
-      chrome.storage.sync.set({'url': window.location.href}, function() 
+      title = document.getElementById("eow-title").innerHTML;
+      description = document.getElementById("eow-description").innerHTML;
+      var stored_object = {};
+      stored_object[window.location.href] = {'t': title, 'd': description};
+      console.log(stored_object);
+      chrome.storage.sync.set(stored_object);
+      chrome.storage.sync.get(window.location.href, function(obj)
       {
-        // Notify that we saved.
-        alert('Settings saved');
-        alert(window.location.href);
+      	console.log(obj);
+      	var retrieved_title = obj['t'];
+      	var retrieved_description = obj['d'];
+      	console.log(window.location.href + "\n" + retrieved_title + "\n" + retrieved_description);
       });
   });
 }
